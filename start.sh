5060a6cee8209516d52720c4819dd10fecc08643
Vince Allen <vince@spotify.com>
2015-06-03 15:03:56 -0400

    Adds shell scripts.

--- start.sh
+++ start.sh
@@ -1,4 +1,9 @@
-cd ~/ref-speaker-Debug
-./esdk-ref-speaker
-cd ~/
-python speaker.py
+#!/bin/bash
+echo "Starting RPI_SPOTIFY_SPEAKER"
+sleep 5
+( cd ~/rpispeaker
+sudo python speaker.py &
+)
+( cd ~/ref-speaker-Debug
+./esdk-ref-speaker &
+)
